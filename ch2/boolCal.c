9a10d28f57bd0170aae40a8491f6735344c69a55
pilgrimS <gravityhe@163.com>
2018-11-20 14:44:54 +0800

    add srand(time(NULL))

--- ch2/boolCal.c
+++ ch2/boolCal.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 void inplace_swap (int *x, int *y){
     *y = *x ^ *y;
     *x = *x ^ *y;
@@ -12,16 +13,28 @@ void reverse_array(int a[] , int cnt){
     }
 }
 
+int random_m(int m){
+    return rand()%m;
+}
+
 void main(){
     int a = 5;
     int b = 7;
     inplace_swap(&a,&b);
     printf("a = %d , b = %d\n" , a , b );
 
-    int c[] = {1,2,3,4,5,6};
-    int cnt = 6;
-    reverse_array(c,cnt);
-    for(int front = 0 ; front < cnt ; front++ ){
-        printf(" %d,",c[front]);
+    srand(time(NULL));
+    int m = 200 ;
+    int len = random_m(m);
+    int c[len];
+    for(int front = 0 ; front < len ; front++ ){
+        c[front] = random_m(100);
+        printf("%d," , c[front]);
     }
+    printf("\n");
+    reverse_array(c,sizeof(c)/sizeof(c[0]));
+    for(int front = 0 ; front < len ; front++ ){
+        printf("%d,",c[front]);
+    }
+    printf("\n");
 }
